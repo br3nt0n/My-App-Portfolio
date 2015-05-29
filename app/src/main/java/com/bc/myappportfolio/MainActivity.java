73af3e483e68f2292201392ee039aa60119aebac
Brenton Currie <brenton.currie@thecloudhub.com>
2015-05-30 07:28:35 +1000

    Initial commit

---
+++ app/src/main/java/com/bc/myappportfolio/MainActivity.java
@@ -0,0 +1,74 @@
+package com.bc.myappportfolio;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonClicked(View view){
+        switch (view.getId()){
+            case R.id.appSpotify:
+                showToastMessage(getString(R.string.toast_app_spotify));
+                break;
+            case R.id.appScores:
+                showToastMessage(getString(R.string.toast_app_scores));
+                break;
+            case R.id.appLibrary:
+                showToastMessage(getString(R.string.toast_app_library));
+                break;
+            case R.id.appBuildItBigger:
+                showToastMessage(getString(R.string.toast_app_builditbigger));
+                break;
+            case R.id.appXYZ:
+                showToastMessage(getString(R.string.toast_app_xyzreader));
+                break;
+            case R.id.appCapstone:
+                showToastMessage(getString(R.string.toast_app_capstone));
+                break;
+            default:
+                showToastMessage("");
+                break;
+        }
+
+    }
+
+    public void showToastMessage(String appName){
+        CharSequence message = getString(R.string.toast_sentence_start) + appName + getString(R.string.toast_sentence_end);
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, message, duration);
+        toast.show();
+    }
+}
